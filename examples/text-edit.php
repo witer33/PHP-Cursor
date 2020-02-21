375bf6f6b25ec54530cee1538dd0c5aaf115d50a
witer33 <leox337@gmail.com>
2020-02-21 11:08:26 +0100

    Create text-edit.php

---
+++ examples/text-edit.php
@@ -0,0 +1,13 @@
+<?php
+
+include "Cursor.php";
+
+echo "TEST"; // print TEST
+
+Cursor::back(4); // move back the cursor
+
+Cursor::clearline(); // clear the space between the cursor and the end of the line
+
+echo "test"; // print test
+
+?>
